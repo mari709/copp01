007b7340e5078204f00822ec6c6d529ef4737f0a
mari <mansumdp@gmail.com>
2020-10-04 21:49:23 -0300

    separa footer y navbar

--- dist/index.php
+++ dist/index.php
@@ -34,51 +34,15 @@ Seguridad::integridadSistema();
 <!----Inicio "body" -->    
 <body class="sb-nav-fixed">
         
-<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <a class="navbar-brand" href="index.php">Coppens</a>
-            <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
-            
-<!---- Navbar Search------------------------------------------------------------------------------------------------------------------->
-<!---- <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0"> ---->
-<!---- <div class="input-group"> ---->
-<!---- <input class="form-control" type="text" placeholder="esto lo vamos a quitar..." aria-label="Search" aria-describedby="basic-addon2" /> ---->
-<!---- <div class="input-group-append"> ---->
-<!---- <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button> ---->
-<!---- </div> ---->
-<!---- </div> ---->
-<!---- </form> ---->
-<!------------------------------------------------------------------------------------------------------------------------------------->            
-            
-            
-<!---- Navbar ------------------------------------------------------------------------------------------------------------------------>
-<!---- Inicio : lista ---->
-<ul class="navbar-nav ml-auto">
+        <!-- NAVBAR -->
+        <?php include("php/includes/navbar.php"); ?>
 
-<!---- Inicio : Elemento #1 ---->                
-<li class="nav-item dropdown">
-
-<a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-             
-<div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-<a class="dropdown-item" href="./login.php">Cerrar sesión</a>
-</div>
-                
-</li>
-<!---- Fin : Elemento #1 ---->
-
-</ul>
-<!---- Fin : lista ---->            
-<!------------------------------------------------------------------------------------------------------------------------------------->            
-       
-       
-        </nav>
         <div id="layoutSidenav">
 
         <!-- INICIO DE SIDEBAR -->
         <?php include("php/includes/sidebar.php"); ?>
             
-
-
+        
             <!--INICIO CONTENIDO PRINCIPAL-->
             <div id="layoutSidenav_content">
                 <main>
@@ -153,13 +117,11 @@ Seguridad::integridadSistema();
                         </div>
                     </div>
                 </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Coppens 2020</div>
-                        </div>
-                    </div>
-                </footer>
+        <!-- FOOTER -->
+            <?php include("php/includes/footer.php"); ?>
+        <!--//FIN FOOTER -->
+
+        
             </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
