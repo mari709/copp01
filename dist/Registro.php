bf775da69ed300a10cc06b884be4fd25370e94d9
JhonLocke10 <mdq-gabriel@hotmail.com>
2020-10-06 15:19:35 -0300

    Update Registro.php

--- dist/Registro.php
+++ dist/Registro.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -11,6 +11,9 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
     </head>
     <body class="bg-primary">
+        <!-- NAVBAR -->
+        <?php include("php/includes/navbar.php"); ?>
+
         <div id="layoutAuthentication">
             <div id="layoutAuthentication_content">
                 <main>
@@ -18,20 +21,31 @@
                         <div class="row justify-content-center">
                             <div class="col-lg-7">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Create Account</h3></div>
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Crear usuario</h3></div>
                                     <div class="card-body">
-                                        
-
-<form>
-
-
-<div class="form-row">
-<div class="col-md-6">
-                                                    
-<div class="form-group">
-<label class="small mb-1" for="inputFirstName">First Name</label>
-<input class="form-control py-4" id="inputFirstName" type="text" placeholder="Enter first name" />
-</div>
+                                        <form>
+                                            <div class="form-row">
+                                                <div class="col-md-6">
+                                                    <fieldset>
+                                                        <legend class="small mb-1">Indique tipo de usuario a crear</legend>
+                                                        <label for="viajante"><input id="viajante"  value="viajante" type="radio" name="grupotipoviajante"> Viajante</label>
+                                                        <label for="administrador"><input id="administrador" value="administrador" type="radio" name="grupotipoviajante"> Administrador</label>
+                                                    </fieldset>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <fieldset>
+                                                        <legend class="small mb-1">Estado del viajante</legend>
+                                                        <label for="eviajanteh"><input id="eviajanteh" value="habilitado" type="radio" name="grupoestadoviajante"> Habilitado</label>
+                                                        <label for="eviajantea"><input id="eviajantea" value="deshabilitado" type="radio" name="grupoestadoviajante"> Deshabilitado</label>
+                                                    </fieldset>
+                                                </div>
+                                            </div>
+                                            <div class="form-row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label class="small mb-1" for="inputFirstName">First Name</label>
+                                                        <input class="form-control py-4" id="inputFirstName" type="text" placeholder="Enter first name" />
+                                                    </div>
                                                 </div>
                                                 <div class="col-md-6">
                                                     <div class="form-group">
@@ -39,9 +53,7 @@
                                                         <input class="form-control py-4" id="inputLastName" type="text" placeholder="Enter last name" />
                                                     </div>
                                                 </div>
-                                            
-</div>
-                                            
+                                            </div>
                                             <div class="form-group">
                                                 <label class="small mb-1" for="inputEmailAddress">Email</label>
                                                 <input class="form-control py-4" id="inputEmailAddress" type="email" aria-describedby="emailHelp" placeholder="Enter email address" />
@@ -60,11 +72,11 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div class="form-group mt-4 mb-0"><a class="btn btn-primary btn-block" href="login.html">Create Account</a></div>
+                                            <div class="form-group mt-4 mb-0"><a class="btn btn-primary btn-block" href="login.html">Agregar usuario</a></div>
                                         </form>
                                     </div>
                                     <div class="card-footer text-center">
-                                        <div class="small"><a href="login.html">Have an account? Go to login</a></div>
+                                        <div class="small"><a href="login.php">Have an account? Go to login</a></div>
                                     </div>
                                 </div>
                             </div>
@@ -73,18 +85,7 @@
                 </main>
             </div>
             <div id="layoutAuthentication_footer">
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2020</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
+                <?php include("php/includes/footer.php"); ?>
             </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
