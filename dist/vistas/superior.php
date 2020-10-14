3481e8a548ff14df50512f4c7d34ac23028b88af
mari <mansumdp@gmail.com>
2020-10-14 16:16:33 -0300

    agrego css para tablas

--- dist/vistas/superior.php
+++ dist/vistas/superior.php
@@ -9,6 +9,7 @@
         <title><?php echo "$title | Coppens";?></title>
         <link href="css/styles.css" rel="stylesheet" />
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+        <link href="css/style-tablas.css" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
     </head>
