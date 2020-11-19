8a17ff1d65e9a372cd65198c96b2ad9509f57052
mari <mansumdp@gmail.com>
2020-11-19 10:53:34 -0300

    correccion de errores del merge

--- dist/vistas/superior.php
+++ dist/vistas/superior.php
@@ -11,7 +11,7 @@
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <link href="css/style-tablas.css" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/js/all.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
    
    	<!---- Inicio : "Estilos" para Tablas ---->
    	<style>
@@ -23,10 +23,7 @@
    	</style>
     <!---- Fin : "Estilos" para Tablas ---->
    
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
     </head>
-
-
 <!-- Inicio "body" -->    
 <body class="sb-nav-fixed">
     
