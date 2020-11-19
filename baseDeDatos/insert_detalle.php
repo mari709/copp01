792839b9e23fe628d6b2a2224da690a3ba49bfed
mari <mansumdp@gmail.com>
2020-11-19 16:14:24 -0300

    inserto datos de la nota pedido en la base de datos

--- baseDeDatos/insert_detalle.php
+++ baseDeDatos/insert_detalle.php
@@ -1,11 +1,19 @@
 <?php
 
-echo "recibiendo datos";
-
+$link = mysqli_connect( "localhost", "root", "")
+or die ("no se ha podido conectar");
+mysqli_select_db($link, "coppens")
+or die("Error al tratar de selecccionar esta base");
 
 $id_producto = $_POST['select-productos'];
+$last = $_POST['last'];
 
-echo $id_producto;
 
+$sql_insert = "INSERT INTO `nota_pedidos_detalle` (`idnpd`, `idnp`, `cant`, `idproducto`, `precio`, `descuento`, `total`, `estado`, `ano_balance`, `idfamilia`, `comision_cobrador`) VALUES 
+                   ('null', '".$last."', 'null', '".$id_producto."', '1111', '2222', '3333', '0', 'null', 'null', 'null')";
+
+$hacer = mysqli_query($link,$sql_insert);
+
+header('Location:../dist/crear-nota-pedido-2.php');
 
 ?>
